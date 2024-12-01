99527a97267e637f969f5e33449dba2e0d4fc476
Komar0ff <Binary.reptile@gmail.com>
2024-12-01 14:06:44 +0300

    chore: update contract

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,44 +1,33 @@
 // SPDX-License-Identifier: UNLICENSE
 
-
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Shitcoin {
-  constructor() {}
-
-  /*OPTIONAL ERC-20 FUNCTIONS*/
-
-  string private _name = "shitcoin";
-  function name() public view returns (string memory) {
-    return _name;
-  }
-
-  string private _shortName = "SHT";
-  function symbol() public view returns (string memory){
-    return _shortName;
-  }
-
-  uint256 private _total = 10000;
-  function totalSupply() public view returns (uint256) {
-    return _total;
-  }
-
-  function decimal() public pure returns (uint8) {
-    return 18;
-  }
-
-
-  /*MUST HAVE ERC-20 FUNCTIONS*/
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 
   mapping(address => uint256) private _balances;
+  mapping(address => mapping(address => uint256)) private _allowances;
+
+  uint256 private _total = 10000;
+
+  constructor() {
+    _balances[msg.sender] = _total; 
+    emit Transfer(address(0), msg.sender, _total);
+  }
 
   function balanceOf(address account) public view returns (uint256){
     return _balances[account];
   }
 
   function transfer(address to, uint256 amount) public returns (bool) {
+    require(amount > 0, "Amount must be greater than zero");
     require(_balances[msg.sender] >= amount, "Not enough tokens");
-    require(to != address(0))
+    unchecked {
+      _balances[msg.sender] -= amount;
+      _balances[to] += amount;
+    }
+    require(to != address(0), "Invalid address");
 
     _balances[msg.sender] -= amount;
     _balances[to] += amount;
@@ -47,12 +36,14 @@ contract Shitcoin {
     return true;
   }
 
-  mapping(address => mapping(address => uint256)) private _allowances;
-
-  function transferFrom(address from, address to, uint256 amount) {
+  function transferFrom(address from, address to, uint256 amount) public returns (bool){
+    require(amount > 0, "Amount must be greater than zero");
     require(_balances[from] >= amount, "Not enough tokens");
+    require(to != address(0), "Invalid address");
     require(_allowances[from][msg.sender] >= amount, "Allowance exceeded");
-    require(to != address(0))
+    if (_allowances[from][msg.sender] != type(uint256).max) {
+      _allowances[from][msg.sender] -= amount;
+    }
 
     _balances[from] -= amount;
     _balances[to] += amount;
@@ -69,8 +60,25 @@ contract Shitcoin {
       return true;
   }
 
-
   function allowance(address owner, address spender) public view returns (uint256) { 
     return _allowances[owner][spender]; 
+  }
+
+  /*OPTIONAL ERC-20 FUNCTIONS*/
+
+  function name() public pure returns (string memory) {
+    return "shitcoin";
+  }
+
+  function symbol() public pure returns (string memory){
+    return "SHT";
+  }
+
+  function decimal() public pure returns (uint8) {
+    return 18;
+  }
+
+  function totalSupply() public view returns (uint256) {
+    return _total;
   }
 }
