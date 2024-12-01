ff267b0cb20ccbe6b7aa0d2c012413ad0ed8f8b3
Komar0ff <Binary.reptile@gmail.com>
2024-12-01 08:50:43 +0300

    chore: minor changes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./tests",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
 };
 
 export default config;
